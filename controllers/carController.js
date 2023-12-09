a82ea09a3c8cfb05d417508b6c0fae16534ec1a8
Krimothiazine <krimouv1995@gmail.com>
2023-12-09 21:05:13 +0100

    Implement create car funtionality and implement update form request

--- controllers/carController.js
+++ controllers/carController.js
@@ -1,5 +1,8 @@
 const asyncHandler = require("express-async-handler");
+const { body, validationResult } = require("express-validator");
 const Car = require("../models/car");
+const Brand = require("../models/brand");
+const Category = require("../models/category");
 
 // Get request to Display list of all cars
 exports.carsList = asyncHandler(async (req, res) => {
@@ -39,4 +42,96 @@ exports.carDeleteGet = asyncHandler(async (req, res) => {
 exports.carDeletePost = asyncHandler(async (req, res) => {
   await Car.findByIdAndDelete(req.body.carId);
   res.redirect("/cars");
+});
+
+// Get request to display create car form
+exports.carCreateGet = asyncHandler(async (req, res) => {
+  const [allBrands, allCategories] = await Promise.all([
+    await Brand.find({}, "name").sort({ name: 1 }).exec(),
+    await Category.find({}, "name").sort({ name: 1 }).exec(),
+  ]);
+
+  res.render("car_form", {
+    title: "Create Car",
+    brands: allBrands,
+    categories: allCategories,
+  });
+});
+
+// Post request to create car
+exports.carCreatePost = [
+  // validate and sanitize fields
+  body("carName", "Car name must not be empty")
+    .trim()
+    .isLength({ min: 1 })
+    .escape(),
+  body("description", "Car description must not be empty")
+    .trim()
+    .isLength({ min: 1 })
+    .escape(),
+  body("price", "Price must not be empty")
+    .isInt({ min: 1 })
+    .withMessage("Price must be greater than 0"),
+  body("numberInStock", "Stock number must be empty")
+    .isInt({ min: 0 })
+    .withMessage("Stock must be equal or greater than 0"),
+  body("brand", "Brand must not be empty").trim().isLength({ min: 1 }).escape(),
+  body("category", "Category must not be empty")
+    .trim()
+    .isLength({ min: 1 })
+    .escape(),
+
+  // process data
+  asyncHandler(async (req, res, next) => {
+    const errors = validationResult(req);
+
+    const car = new Car({
+      brand: req.body.brand,
+      name: req.body.carName,
+      description: req.body.description,
+      price: req.body.price,
+      numberInStock: req.body.numberInStock,
+      category: req.body.category,
+    });
+
+    if (!errors.isEmpty()) {
+      const [allBrands, allCategories] = await Promise.all([
+        await Brand.find({}, "name").sort({ name: 1 }).exec(),
+        await Category.find({}, "name").sort({ name: 1 }).exec(),
+      ]);
+
+      res.render("car_form", {
+        title: "Create Car",
+        brands: allBrands,
+        categories: allCategories,
+        car: car,
+        errors: errors.array(),
+      });
+    } else {
+      await car.save();
+      res.redirect(car.url);
+    }
+  }),
+];
+
+// Get request to display update form
+exports.carUpdateGet = asyncHandler(async (req, res, next) => {
+  const [allBrands, allCategories, car] = await Promise.all([
+    await Brand.find({}, "name").sort({ name: 1 }).exec(),
+    await Category.find({}, "name").sort({ name: 1 }).exec(),
+    await Car.findById(req.params.id).exec(),
+  ]);
+
+  if (car == null) {
+    const err = new Error("Car not Found");
+    err.status = 404;
+    return next(err);
+  }
+  console.log("car:", car);
+  res.render("car_form", {
+    title: "Update Car",
+    brands: allBrands,
+    categories: allCategories,
+    car: car,
+  });
 });
--- routes/car.js
+++ routes/car.js
@@ -3,14 +3,10 @@ const router = express.Router();
 const carController = require("../controllers/carController");
 
 // GET request to create car
-router.get("/car/create", (req, res) => {
-  res.send("get request to create car");
-});
+router.get("/car/create", carController.carCreateGet);
 
 // POST request to create car
-router.post("/car/create", (req, res) => {
-  res.send("post request to create car");
-});
+router.post("/car/create", carController.carCreatePost);
 
 // GET request to delete car
 router.get("/car/:id/delete", carController.carDeleteGet);
@@ -19,9 +15,7 @@ router.get("/car/:id/delete", carController.carDeleteGet);
 router.post("/car/:id/delete", carController.carDeletePost);
 
 // GET request to update car
-router.get("/car/:id/update", (req, res) => {
-  res.send(`get request to update: ${req.params.id}`);
-});
+router.get("/car/:id/update", carController.carUpdateGet);
 
 // POST request to update car
 router.post("/car/:id/update", (req, res) => {
